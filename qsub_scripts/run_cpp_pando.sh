a29cf4c1f0c063ddbf4025bb024e38de1ffd65a7
Joseph Azofeifa <azofeifa@pando-3.(none)>
2015-07-12 21:41:36 -0600

    qsub updates

--- qsub_scripts/run_cpp_pando.sh
+++ qsub_scripts/run_cpp_pando.sh
@@ -41,10 +41,10 @@ fi
 src=/Users/azofeifa/Lab/EMG/CPP_src/EMGU
 config_file=/Users/azofeifa/Lab/EMG/cpp_config_files/model_config.txt
 formatted_in_file=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/fs_only_merged_32.tsv
-out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/cpp_model_fits
+out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/cpp_model_fits_2/
 maxK=5
 rounds=64
 
-$src $config_file  -i $formatted_in_file -o $out_directory -np $PBS_ARRAYID -rounds $rounds
+$src $config_file  -i $formatted_in_file -o $out_directory -np $PBS_ARRAYID -rounds $PBS_ARRAYID 
 
 
