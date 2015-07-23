2c8e446e9046f54625e212b87114408eb397aaaa
azofeifa <jgazofeifa@gmail.com>
2015-07-23 10:59:17 -0600

    backups

--- qsub_scripts/run_cpp_pando.sh
+++ qsub_scripts/run_cpp_pando.sh
@@ -40,8 +40,8 @@ fi
 
 src=/Users/azofeifa/Lab/EMG/CPP_src/EMGU
 config_file=/Users/azofeifa/Lab/EMG/cpp_config_files/model_config.txt
-formatted_in_file=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/EMG_formmated_files/Nutlin_ND_intervals_formatted.tsv
-out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/Nutlin_ND_intervals_model_fits_3/
+formatted_in_file=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/EMG_formmated_files/DMSO_ND_intervals_formatted.tsv
+out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/DMSO_ND_intervals_model_fits_2/
 chr=chr$PBS_ARRAYID
 $src $config_file  -i $formatted_in_file -o $out_directory -chr $chr
 
