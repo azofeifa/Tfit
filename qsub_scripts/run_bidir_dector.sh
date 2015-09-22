750092c7a4e0058f4613407b08939a4447e35ad5
azofeifa <jgazofeifa@gmail.com>
2015-09-21 18:49:16 -0600

    updates

--- qsub_scripts/run_bidir_dector.sh
+++ qsub_scripts/run_bidir_dector.sh
@@ -1,49 +1,46 @@
-#Name the job
+#PBS -S /bin/bash
+
 #PBS -N DMSO2_3
+###PBS -q shortgb
 
-#PBS -l nodes=7:ppn=32,walltime=24:00:00,mem=100gb
-### Allocate the amount of memory needed
-
-### Set your expected walltime
-###PBS -q shortmem
-### Setting to mail when the job is complete
-#PBS -e /Users/azofeifa/qsub_errors/EMG/                                        
-#PBS -o /Users/azofeifa/qsub_stdo/EMG/  
-
-### Set your email address
 #PBS -m ae
-#PBS -M jgazofeifa@gmail.com
+#PBS -M joseph.azofeifa@colorado.edu
 
-#PBS -W x=nmatchpolicy=exactnode
+#PBS -e /Users/azofeifa/qsub_errors/EMG/
+#PBS -o /Users/azofeifa/qsub_stdo/EMG/
 
-### Choose your shell 
-#PBS -S /bin/sh
-### Pass enviroment variables to the job
-#PBS -V
+#PBS -l walltime=05:00:00
 
+#PBS -l nodes=16:ppn=1:mem=2gb
 
+#PBS -W x=HOSTLIST:node-32,node-33,node-34,node-35,node-36,node-37,node-38,node-39,node-40,node-41,node-42,node-43,node-44,node-45,node-46,node-47
+
+#PBS -W x=FLAGS:ADVRES:joey.0
+
+# -- OpenMP environment variables --
+OMP_NUM_THREADS=32
+export OMP_NUM_THREADS
+module load gcc_4.9.2
+module load mpich_3.1.4
 
 ### ===================
 ### what machine?
 ### ===================
 
-module load gcc_4.9.2 ###load most recent gcc compiler
-module load openmpi_1.6.4
-cd $PBS_O_WORKDIR
 
 src=/Users/azofeifa/Lab/EMG/CPP_src/EMGU
 config_file=/Users/azofeifa/Lab/EMG/cpp_config_files/bidir_config.txt
 bedgraph_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/bed_graph_files/
 interval_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/FStitch/
 out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/DMSO2_3_model_fits/
+tmp_log_directory=/Users/azofeifa/EMG_log_files/
 
 forward_bedgraph=DMSO2_3.pos.BedGraph
 reverse_bedgraph=DMSO2_3.neg.BedGraph
 interval_file=DMSO2_3_Nutlin2_3_merged_FStitch.bed
 # -- OpenMP environment variables --
-OMP_NUM_THREADS=$PBS_NUM_PPN
-export OMP_NUM_THREADS
-# -- program invocation here --
-chrom=chr1
 
-mpirun --npernode 1 $src $config_file -i ${bedgraph_directory}$forward_bedgraph -j ${bedgraph_directory}$reverse_bedgraph -f ${interval_directory}$interval_file -chr $chrom   -o $out_directory 
+# -- program invocation here --
+chrom=all
+
+mpirun $src $config_file -i ${bedgraph_directory}$forward_bedgraph -j ${bedgraph_directory}$reverse_bedgraph -f ${interval_directory}$interval_file -chr $chrom   -o $out_directory -log_out $tmp_log_directory
