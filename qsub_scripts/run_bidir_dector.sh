4f6c4c6835f2678b6ce4dacb601438bdad35e751
Joseph Azofeifa <azofeifa@vieques.colorado.edu>
2015-09-21 22:32:32 -0600

    asdf

--- qsub_scripts/run_bidir_dector.sh
+++ qsub_scripts/run_bidir_dector.sh
@@ -1,7 +1,7 @@
 #Name the job
-#PBS -N DMSO2_3
+#PBS -N Hah2011
 
-#PBS -l nodes=7:ppn=32,walltime=24:00:00,mem=100gb
+#PBS -l nodes=12:ppn=32,walltime=24:00:00,mem=150gb
 ### Allocate the amount of memory needed
 
 ### Set your expected walltime
@@ -33,17 +33,22 @@ cd $PBS_O_WORKDIR
 
 src=/Users/azofeifa/Lab/EMG/CPP_src/EMGU
 config_file=/Users/azofeifa/Lab/EMG/cpp_config_files/bidir_config.txt
-bedgraph_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/bed_graph_files/
+#bedgraph_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/bed_graph_files/
+bedgraph_directory=/Users/azofeifa/Lab/gro_seq_files/Hah2011/bedgraph_files/
 interval_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/FStitch/
-out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/DMSO2_3_model_fits/
+#out_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/EMG_out_files/
+out_directory=/Users/azofeifa/Lab/gro_seq_files/Hah2011/EMG_out_files/model_fits_control/
+tmp_log_directory=/Users/azofeifa/EMG_log_files/
 
-forward_bedgraph=DMSO2_3.pos.BedGraph
-reverse_bedgraph=DMSO2_3.neg.BedGraph
+forward_bedgraph=allCore2011.sorted.pos.BedGraph
+reverse_bedgraph=allCore2011.sorted.neg.BedGraph
 interval_file=DMSO2_3_Nutlin2_3_merged_FStitch.bed
 # -- OpenMP environment variables --
 OMP_NUM_THREADS=$PBS_NUM_PPN
 export OMP_NUM_THREADS
+# -- parameters to EMGU these will overwrite config file --
+chrom=all
+rounds=25
+fp=10
 # -- program invocation here --
-chrom=chr1
-
-mpirun --npernode 1 $src $config_file -i ${bedgraph_directory}$forward_bedgraph -j ${bedgraph_directory}$reverse_bedgraph -f ${interval_directory}$interval_file -chr $chrom   -o $out_directory 
+mpirun --npernode 1 $src $config_file -i ${bedgraph_directory}$forward_bedgraph -j ${bedgraph_directory}$reverse_bedgraph -f ${interval_directory}$interval_file -chr $chrom -rounds $rounds -fp $fp   -o $out_directory -log_out $tmp_log_directory
