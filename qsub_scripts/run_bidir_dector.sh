09d0e32b1a8b61fd160ab34f69c9a4186a4692f3
azofeifa <jgazofeifa@gmail.com>
2015-09-27 20:36:18 -0600

    uppdates

--- qsub_scripts/run_bidir_dector.sh
+++ qsub_scripts/run_bidir_dector.sh
@@ -1,6 +1,6 @@
 #PBS -S /bin/bash
 
-#PBS -N Li2013_MCF7_siSMC3_EtoH
+#PBS -N Allen2014_Nutlin30_30
 
 #PBS -m ae
 #PBS -M joseph.azofeifa@colorado.edu
@@ -9,8 +9,8 @@
 #PBS -o /Users/azofeifa/qsub_stdo/EMG/
 
 #PBS -l walltime=12:00:00
-#PBS -l nodes=10:ppn=64
-#PBS -l mem=700gb
+#PBS -l nodes=40:ppn=64
+#PBS -l mem=800gb
 
 hostlist=$( cat $PBS_NODEFILE | sort | uniq | tr '\n' ',' | sed -e 's/,$//' )
 # -- OpenMP environment variables --
@@ -23,11 +23,9 @@ module load mpich_3.1.4
 #paths to config and src
 src=/Users/azofeifa/Lab/EMG/CPP_src/EMGU
 config_file=/Users/azofeifa/Lab/EMG/cpp_config_files/bidir_config.txt
-#================================================================
-#Interval Files not needed yet
-interval_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/FStitch/
 tmp_log_directory=/Users/azofeifa/EMG_log_files/
-interval_file=DMSO2_3_Nutlin2_3_merged_FStitch.bed
+hg18=/Users/azofeifa/Lab/genome_files/hg18_no_anntation.bed
+hg19=/Users/azofeifa/Lab/genome_files/hg19_no_anntation.bed
 #================================================================
 #bedgraph directories
 pubgro=/projects/dowellLab/groseq/pubgro/
@@ -35,9 +33,11 @@ bedgraph_directory=/Users/azofeifa/Lab/gro_seq_files/HCT116/bed_graph_files/ #al
 Core2014=${pubgro}Core2014/bedGraph_from_bw/
 Danko2014=${pubgro}Danko2014/bedGraph_from_bw/
 Li2013=${pubgro}Li2013/forFStitch/
-Liu2013=${pubgro}Liu2013/
+Liu2013=${pubgro}Liu2013/bedGraph_from_bw/
+Puc2015=${pubgro}Puc2015/bedGraphs_from_bedGraph/
 Meng2014b=${pubgro}Meng2014b/
 Zhang2015=${pubgro}Zhang2015/
+Allen2014=/Users/azofeifa/Lab/gro_seq_files/Allen2014/bed_graph_files/
 #================================================================
 #bedgraph files
 
@@ -66,34 +66,64 @@ Li2013_MCF7_siSMC3_E2_reverse=GSM1115999_Groseq-MCF7-siSMC3-E2.minusstrand.bigWi
 Li2013_MCF7_siSMC3_EtoH_forward=GSM1116000_Groseq-MCF7-siSMC3-EtoH.plusstrand.bigWig.bedGraph
 Li2013_MCF7_siSMC3_EtoH_reverse=GSM1116000_Groseq-MCF7-siSMC3-EtoH.minusstrand.bigWig.bedGraph.plus.bedGraph
 
+Liu2013_siBRD4_forward=siBRD4-1-re1_2-HEK293T-forward.bedGraph
+Liu2013_siBRD4_reverse=siBRD4-1-re1_2-HEK293T-reverse.bedGraph
+Liu2013_siCTL_forward=siCTL-re1_2-HEK293T-forward.bedGraph
+Liu2013_siCTL_reverse=siCTL-re1_2-HEK293T-reverse.bedGraph
+Liu2013_siJMJD6_forward=siJMJD6-1-re1_2-HEK293-forward.bedGraph
+Liu2013_siJMJD6_reverse=siJMJD6-1-re1_2-HEK293-reverse.bedGraph
 
+Puc2015_si_control_vehicle_forward=siControl_1h_vehicle_hg18_forward.bedGraph
+Puc2015_si_control_vehicle_reverse=siControl_1h_vehicle_hg18_reverse.bedGraph
+Puc2015_siMRE11_dht_forward=siMRE11_1h_dht_hg18_forward.bedGraph
+Puc2015_siMRE11_dht_reverse=siMRE11_1h_dht_hg18_reverse.bedGraph
+Puc2015_siMRE11_vehicle_forward=siMRE11_1h_vehicle_hg18_forward.bedGraph
+Puc2015_siMRE11_vehicle_reverse=siMRE11_1h_vehicle_hg18_reverse.bedGraph
+Puc2015_siTOP1_vehicle_forward=siTOP1_1h_vehicle_hg18_forward.bedGraph
+Puc2015_siTOP1_vehicle_reverse=siTOP1_1h_vehicle_hg18_reverse.bedGraph
+Puc2015_siTOP1_dht_forward=siTOP1_1h_dht_hg18_forward.bedGraph
+Puc2015_siTOP1_dht_reverse=siTOP1_1h_dht_hg18_reverse.bedGraph
 
-#================================================================
-#Final Directory and bedgraph directories
-bedgraph_directory=$Li2013
-forward=$Li2013_MCF7_siSMC3_EtoH_forward
-reverse=$Li2013_MCF7_siSMC3_EtoH_reverse
+Allen2014_DMSO2_3_forward=DMSO2_3.pos.BedGraph
+Allen2014_DMSO2_3_reverse=DMSO2_3.neg.BedGraph
+Allen2014_DMSO1027_1212_forward=DMSO1027_1212.pos.BedGraph
+Allen2014_DMSO1027_1212_reverse=DMSO1027_1212.pneg.BedGraph
+Allen2014_DMSO1hr101911_forward=DMSO1hr101911_lane8.pos.BedGraph
+Allen2014_DMSO1hr101911_reverse=DMSO1hr101911_lane8.pneg.BedGraph
+Allen2014_DMSOp531027_1212_forward=DMSOp531027_1212.pos.BedGraph
+Allen2014_DMSOp531027_1212_reverse=DMSOp531027_1212.pneg.BedGraph
+Allen2014_Ma6_NoIndex_forward=Ma6_NoIndex_L008_R1_001.pos.BedGraph
+Allen2014_Ma6_NoIndex_reverse=Ma6_NoIndex_L008_R1_001.pneg.BedGraph
+Allen2014_Nutlin2_3_forward=Nutlin2_3.sorted.pos.BedGraph
+Allen2014_Nutlin2_3_reverse=Nutlin2_3.sorted.neg.BedGraph
+Allen2014_Nutlin30_30_forward=Nutlin30_30.pos.BedGraph
+Allen2014_Nutlin30_30_reverse=Nutlin30_30.pneg.BedGraph
+
 
 #================================================================
 #out and temp directory
 EMG_out=/Users/azofeifa/Lab/gro_seq_files/
-Core2014_out=${EMG_out}Core_2014/
-Danko_2014_out=${EMG_out}Danko_2014/
-HCT116_out=${EMG_out}HCT116/
-Li2013_out=${EMG_out}Li2013/
-Liu2013_out=${EMG_out}Liu2013/
-Meng2014b_out=${EMG_out}Meng2014b/
-Zhang2015_out=${EMG_out}Zhang2015/
+Core2014_out=${EMG_out}Core_2014/EMG_out_files/
+Danko_2014_out=${EMG_out}Danko_2014/EMG_out_files/
+Allen2014_out=${EMG_out}Allen2014/EMG_out_files/
+Li2013_out=${EMG_out}Li2013/EMG_out_files/
+Liu2013_out=${EMG_out}Liu2013/EMG_out_files/
+Puc2015_out=${EMG_out}Puc2015/EMG_out_files/
+Meng2014b_out=${EMG_out}Meng2014b/EMG_out_files/
+Zhang2015_out=${EMG_out}Zhang2015/EMG_out_files/
 #================================================================
 #FINAL PARAMETERS
-out_directory=$Li2013_out
 
-
+bedgraph_directory=$Allen2014
+forward=$Allen2014_Nutlin30_30_forward
+reverse=$Allen2014_Nutlin30_30_reverse
+out_directory=$Allen2014_out
+genome=$hg19
 
 
 
 #================================================================
 #calling command
 cmd="mpirun -np $PBS_NUM_NODES -hosts ${hostlist}"
-$cmd $src $config_file -i ${bedgraph_directory}$forward -j ${bedgraph_directory}$reverse -f ${interval_directory}$interval_file   -o $out_directory -log_out $tmp_log_directory 
+$cmd $src $config_file -i ${bedgraph_directory}$forward -j ${bedgraph_directory}$reverse -f ${interval_directory}$interval_file   -o $out_directory -log_out $tmp_log_directory -N $PBS_JOBNAME -nf $genome
 #================================================================
