1068f4d805502b9a86ad778b62a76cd0befc2668
azofeifa <jgazofeifa@gmail.com>
2015-11-09 17:37:11 -0700

    updates

--- CPP_src/load.cpp
+++ CPP_src/load.cpp
@@ -882,23 +882,30 @@ map<string, vector<merged_interval*> > segments_to_merged_intervals(map<string, 
 		    G[c->first] 	= bubble_sort(G[c->first]);
 
 		    j 				= 0;
+		    merged_interval * I = new  merged_interval(G[c->first][0].start, G[c->first][0].stop, G[c->first][0], j);
+		    I->parameters 	= G[c->first][0].parameters;
 		    N 				= G[c->first].size();
-		    i 				= 0;
+		    i 				= 1;
 		    bool inserted 	= false;
 		    while (i < N){
-		    	merged_interval * I = new  merged_interval(G[c->first][0].start, G[c->first][0].stop, G[c->first][0], j);
-			    I->parameters 	= G[c->first][0].parameters;
 		    	while (i<N and G[c->first][i].start < I->stop and G[c->first][i].stop > I->start ){
 		    		I->update(G[c->first][i]);
 		    		i++;
 		    	}
 		    	A[c->first].push_back(I);
+		    	inserted=true;
 		    	if (i < N){
 		    		I 	= new merged_interval(G[c->first][i].start, G[c->first][i].stop, G[c->first][i], j);
 				    I->parameters 	= G[c->first][i].parameters;
+
+		    		inserted=false;
+		    		j++;
 		    	}
-		    	i++;
+		    //	i++;
 		    }
+			if (not inserted){
+				A[c->first].push_back(I);		    	
+			}
 		}
 	}
 	return A;
@@ -1665,6 +1672,7 @@ vector<segment* > insert_bedgraph_to_segment_single(map<string, vector<segment *
 
 vector<segment* > insert_bedgraph_to_segment_joint(map<string, vector<segment *> > A , 
 	string forward, string reverse, int rank){
+	
 	//want instead to make this interval tree
 	map<string, vector<merged_interval*> > merged_FSI 	= segments_to_merged_intervals( A);
 	map<string, interval_tree *> AT;
