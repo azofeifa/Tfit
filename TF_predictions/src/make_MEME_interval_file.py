8d0571237c360411d38193b88d0f26269f85832f
azofeifa <jgazofeifa@gmail.com>
2015-11-14 10:22:08 -0700

    updates

--- TF_predictions/src/make_MEME_interval_file.py
+++ TF_predictions/src/make_MEME_interval_file.py
@@ -36,11 +36,11 @@ def iterate(root, out):
 		PATH 	= root+"/"+TF_DIR+"/peak_files/outfiles/MEME/"
 		FHW 	= open(out+TF_DIR, "w")
 		i 		= 1
-		print os.path.exists(PATH), PATH
 		if os.path.exists(PATH):
 
 			for fimo_dir in os.listdir(PATH):
 				if fimo_dir[:8]=="fimo_out" and os.path.exists(fimo_dir.split("_")[-1]+ "_fimo_out"): 
+					print fimo_dir
 					ChIP_MOTIF 	= PATH+"/" +fimo_dir + "/fimo.txt"
 					MOITF_ONLY 	= PATH+"/" +fimo_dir.split("_")[-1]+ "_fimo_out/fimo.txt"
 					i 			= write_filter_etc(ChIP_MOTIF, MOITF_ONLY , FHW, i)
